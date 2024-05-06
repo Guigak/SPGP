7595dd6d1e3e511e2e4076f7eaa1e4017765a5e6
Guigak <97238914+Guigak@users.noreply.github.com>
2024-05-06 23:20:58 +0900

    Add packages and move MainActivity
    Add packages(rhythmstair, framework) and move MainActivity

--- RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/MainActivity.java
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/MainActivity.java
@@ -1,8 +1,10 @@
-package kr.ac.tukorea.rhythmstair;
+package kr.ac.tukorea.rhythmstair.rhythmstair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import kr.ac.tukorea.rhythmstair.R;
 
 public class MainActivity extends AppCompatActivity {
 
