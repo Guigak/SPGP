39197175bfbdf9bc9b147d7e00e6552dcabdbcfb
Guigak <97238914+Guigak@users.noreply.github.com>
2024-05-09 05:04:21 +0900

    Add GameActivity class
    Add GameActivity class

--- RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/framework/view/GameView.java
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/framework/view/GameView.java
@@ -1,9 +1,27 @@
 package kr.ac.tukorea.rhythmstair.framework.view;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.view.View;
 
-public class GameView {
+public class GameView extends View {
     private static final String TAG = GameView.class.getSimpleName();
 
+    public GameView(Context context) {
+        super(context);
+    }
+
     public static Resources res;
+
+    public void onBackPressed() {
+    }
+
+    public void pauseGame() {
+    }
+
+    public void resumeGame() {
+    }
+
+    public void destroyGame() {
+    }
 }
