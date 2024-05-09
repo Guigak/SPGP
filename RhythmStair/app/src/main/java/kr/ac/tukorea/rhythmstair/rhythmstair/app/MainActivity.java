2d6359aa3b97508af4068cf96572527767f73738
Guigak <97238914+Guigak@users.noreply.github.com>
2024-05-09 12:10:10 +0900

    Modify MainActivity class
    Modify MainActivity class about staring RhythmStairAcitivity

---
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/app/MainActivity.java
@@ -0,0 +1,33 @@
+package kr.ac.tukorea.rhythmstair.rhythmstair.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.rhythmstair.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startActivity(new Intent(this, RhythmStairActivity.class));
+        }
+        return super.onTouchEvent(event);
+    }
+}
