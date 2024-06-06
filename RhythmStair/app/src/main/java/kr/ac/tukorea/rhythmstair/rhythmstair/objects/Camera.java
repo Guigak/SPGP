835a7802c06943202a049976f9909a4140673815
Guigak <97238914+Guigak@users.noreply.github.com>
2024-06-06 16:06:57 +0900

    Add Camera class
    Add Camera class

---
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/objects/Camera.java
@@ -0,0 +1,28 @@
+package kr.ac.tukorea.rhythmstair.rhythmstair.objects;
+
+public class Camera {
+    private static final String TAG = Camera.class.getSimpleName();
+
+    public static int dx = 0, dy = 0;
+
+    public Camera() {
+        clear();
+    }
+
+    public void clear() {
+        dx = 0;
+        dy = 0;
+    }
+
+    public void moveLeft() {
+        dx -= 1;
+    }
+
+    public void moveRight() {
+        dx += 1;
+    }
+
+    public void moveUp() {
+        dy += 1;
+    }
+}
