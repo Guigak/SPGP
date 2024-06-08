1cad9c6858f54afbbf30c45d654eccc8f5ffc4d3
Guigak <97238914+Guigak@users.noreply.github.com>
2024-06-09 02:30:35 +0900

    Modify PlayScene class
    Modify PlayScene class for test charcter

--- RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/game/PlayScene.java
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/game/PlayScene.java
@@ -8,20 +8,33 @@ import kr.ac.tukorea.rhythmstair.framework.scene.Scene;
 import kr.ac.tukorea.rhythmstair.framework.view.Metrics;
 import kr.ac.tukorea.rhythmstair.rhythmstair.objects.Background;
 import kr.ac.tukorea.rhythmstair.rhythmstair.objects.Camera;
+import kr.ac.tukorea.rhythmstair.rhythmstair.objects.Character;
+import kr.ac.tukorea.rhythmstair.rhythmstair.objects.Stair;
 
 public class PlayScene extends Scene {
     private static final String TAG = PlayScene.class.getSimpleName();
 
     private final Camera camera = new Camera();
 
+    private Character character = null;
+
     public enum Layer {
-        backgroung, COUNT
+        bg, cam, character, stair, COUNT
     }
 
-    public PlayScene() {
+    public PlayScene(int map, int diff, int chr) {
         initLayers(Layer.COUNT);
 
-        add(Layer.backgroung, new Background(R.mipmap.hyperspace_rhythm_bg, 0.5f, 0.5f, 1.0f, 1.0f));
+        add(Layer.bg, new Background(R.mipmap.hyperspace_rhythm_bg, 0.5f, 0.5f, 1.0f, 1.0f));
+
+        add(Layer.cam, camera);
+
+        character = new Character(chr);
+        add(Layer.character, character);
+
+        for (int i = 0; i < 20; ++i) {
+            add(Layer.stair, new Stair(i, i, Stair.Maps.Hyperspace_Rhythm, i % 3, (float)(i)));
+        }
     }
 
     @Override
@@ -31,7 +44,19 @@ public class PlayScene extends Scene {
 
     @Override
     public boolean onTouch(MotionEvent event) {
-        camera.moveUp();
+        float[] tempPoints = Metrics.fromScreen(event.getX(), event.getY());
+        float inputX = tempPoints[0] / Metrics.width;
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (inputX > 0.5f) {
+                character.move();
+                camera.moveRight();
+            }
+            else {
+                character.move();
+                camera.moveLeft();
+            }
+        }
         return true;
     }
 }
