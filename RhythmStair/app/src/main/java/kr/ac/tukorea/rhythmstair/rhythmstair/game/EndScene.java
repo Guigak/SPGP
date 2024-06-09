177e70d0c6b33c1d90d98448f3d7819a718382de
Guigak <97238914+Guigak@users.noreply.github.com>
2024-06-10 03:40:20 +0900

    Add EndScene class
    Add EndScene class for test

---
+++ RhythmStair/app/src/main/java/kr/ac/tukorea/rhythmstair/rhythmstair/game/EndScene.java
@@ -0,0 +1,38 @@
+package kr.ac.tukorea.rhythmstair.rhythmstair.game;
+
+import android.view.MotionEvent;
+
+import kr.ac.tukorea.rhythmstair.R;
+import kr.ac.tukorea.rhythmstair.framework.objects.Button;
+import kr.ac.tukorea.rhythmstair.framework.objects.HorizontalScrollableSprites;
+import kr.ac.tukorea.rhythmstair.framework.resource.SoundPlayer;
+import kr.ac.tukorea.rhythmstair.framework.scene.Scene;
+import kr.ac.tukorea.rhythmstair.framework.view.Metrics;
+
+public class EndScene extends Scene {
+    private static final String TAG = EndScene.class.getSimpleName();
+
+    public enum Layer {
+        titles, difficulty, characters, button, COUNT
+    }
+
+    public EndScene() {
+        initLayers(Layer.COUNT);
+    }
+
+    @Override
+    public void update(float elapsedSeconds) {
+        super.update(elapsedSeconds);
+    }
+
+    @Override
+    public boolean onTouch(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Scene.pop();
+            Scene.pop();
+            return true;
+        }
+
+        return false;
+    }
+}
